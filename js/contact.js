c58493de97f1aebc2cdc704a22ab9c37ec14f186
Frederikke <frny0001@stud.kea.dk>
2025-02-20 12:31:20 +0100

    changes

--- js/contact.js
+++ js/contact.js
@@ -6,6 +6,27 @@ document.querySelector('#frmContact').addEventListener('submit', (e) => {
     const email = e.target.txtEmail.value;
     const comments = e.target.txtComments.value;
 
+    const modal = document.querySelector('#contact_message');
+    modal.querySelector('#name').innerText = name;
+    modal.querySelector('#email').innerText = email;
+    modal.querySelector('#comments').innerText = comments;
+
+    modal.showModal();
+    modal.focus();
+
+    modal.querySelector('.close').addEventListener('click', function() {
+        this.parentElement.parentElement.close();
+    });
+
+    /*faster
+    modal.querySelector('.close').addEventListener('click', () => {
+        modal.close();
+    });*/
+
+    modal.addEventListener('close', () => {
+        window.location.replace('index.html');
+    });
+/*
     const submittedInfo = `
         Thank you for your message, we will be back to you shortly.
 
@@ -16,8 +37,8 @@ document.querySelector('#frmContact').addEventListener('submit', (e) => {
     `;
 
     alert(submittedInfo);
-    
+*/
 /*redirecting to home page when form is submitted */
-    window.location.replace('index.html');
+   // window.location.replace('index.html');
 });
 
