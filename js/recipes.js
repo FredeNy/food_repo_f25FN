8cb74076f3e4c3c3c1d1e229f7f041a010b91ede
Frederikke <frny0001@stud.kea.dk>
2025-02-20 10:07:00 +0100

    flex changes

--- js/recipes.js
+++ js/recipes.js
@@ -2,9 +2,10 @@ import { BASE_URL } from './info.js';
 
 const DEFAULT_RECIPES = 10;
 
-const showRandomRecipes = (numRecipes = DEFAULT_RECIPES) => {
+const showRandomRecipesWithInnerHTML = (numRecipes = DEFAULT_RECIPES) => {
 
     for (let index = 0; index < numRecipes; index++) {
+
         fetch(`${BASE_URL}/random.php`)
         .then(response => response.json())
         .then(data => {
@@ -25,4 +26,108 @@ const showRandomRecipes = (numRecipes = DEFAULT_RECIPES) => {
         .catch(error => console.log(error));
     }
 };
+
+const showRandomRecipesWithInnerHTMLAndOnePageRefresh = async (numRecipes = DEFAULT_RECIPES) => {
+
+    let recipeList = '';
+    for (let index = 0; index < numRecipes; index++) {
+
+        await fetch(`${BASE_URL}/random.php`)
+        .then(response => response.json())
+        .then(data => {
+            data = data.meals[0];
+            recipeList += `
+                <article>
+                    <header>
+                        <h2>${data.strMeal}</h2>
+                    </header>
+                    <img src="${data.strMealThumb}/preview" alt="${data.strMeal}">
+                    <div>
+                        <p class="pill">${data.strCategory}</p>
+                        <p class="pill">${data.strArea}</p>
+                    </div>
+                </article>
+            `;
+        })
+        .catch(error => console.log(error));
+
+        document.querySelector('#recipe-list').innerHTML = recipeList;
+    }
+};
+
+///correct way, because it doenst inject html directly into the code (better for preventing hacking)
+//avoid innerHTML because it makes it easy to hack a page
+const showRandomRecipesWithCreateElement = async (numRecipes = DEFAULT_RECIPES) => {
+
+    const fragment = document.createDocumentFragment();
+    for (let index = 0; index < numRecipes; index++) {
+
+        await fetch(`${BASE_URL}/random.php`)
+        .then(response => response.json())
+        .then(data => {
+            data = data.meals[0];
+
+            const h2 = document.createElement('h2');
+            h2.innerText = data.strMeal;
+
+            const header = document.createElement('header');
+            header.append(h2);
+
+            const img = document.createElement('img');
+            img.setAttribute('src', `${data.strMealThumb}/preview`);
+            img.setAttribute('alt', data.strMeal);
+
+            const pCategory = document.createElement('p');
+            pCategory.classList.add('pill');
+            pCategory.innerText = data.strCategory;
+
+            const pArea = document.createElement('p');
+            pArea.classList.add('pill');
+            pArea.innerText = data.strArea;
+            
+            const div = document.createElement('div');
+            div.append(pCategory);
+            div.append(pArea);
+
+            const article = document.createElement('article');
+            article.append(header);
+            article.append(img);
+            article.append(div);
+
+            fragment.append(article);
+        })
+        .catch(error => console.log(error));
+    }
+    document.querySelector('#recipe-list').append(fragment);
+};
+
+//template way datafetching from API
+const showRandomRecipes = async (numRecipes = DEFAULT_RECIPES) => {
+
+    const fragment = document.createDocumentFragment();
+    for (let index = 0; index < numRecipes; index++) {
+
+        await fetch(`${BASE_URL}/random.php`)
+        .then(response => response.json())
+        .then(data => {
+            data = data.meals[0];
+
+            const card = document.querySelector('#recipe-card').content.cloneNode(true);
+
+            card.querySelector('h2').innerText = data.strMeal;
+
+            const img = card.querySelector('img');
+            img.setAttribute('src', `${data.strMealThumb}/preview`);
+            img.setAttribute('alt', data.strMeal);
+
+            card.querySelector('.pill:first-of-type').innerText = data.strCategory;
+            card.querySelector('.pill:last-of-type').innerText = data.strArea;
+
+            fragment.append(card);
+        })
+        .catch(error => console.log(error));
+    }
+    document.querySelector('#recipe-list').append(fragment);
+};
+
 showRandomRecipes();
